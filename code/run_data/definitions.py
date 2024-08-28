7b5887cdaa1511fa652708c3febbb566b6c2cbed
LiekeVanSon <lieke_van_son@hotmail.com>
2024-08-28 09:15:09 -0400

    cleaning up the total mass evolved per Z function

--- code/run_data/definitions.py
+++ code/run_data/definitions.py
@@ -5,39 +5,39 @@ from astropy.table import Table
 
 sim_flags_dict = {
       # Fiducial 
-    "OldWinds_RemFryer2012": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss VINK2001 --VMS-mass-loss VINK2011 --RSG-mass-loss NJ90 --WR-mass-loss BELCZYNSKI2010 \
+    "OldWinds_RemFryer2012": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss-prescription VINK2001 --VMS-mass-loss-prescription VINK2011 --RSG-mass-loss-prescription NJ90 --WR-mass-loss-prescription BELCZYNSKI2010 \
  --remnant-mass-prescription FRYER2012 --kick-magnitude-distribution MAXWELLIAN ",
       # Old winds with Muller mandel rem
-    "OldWinds_RemMullerMandel": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss VINK2001 --VMS-mass-loss VINK2011 --RSG-mass-loss NJ90 --WR-mass-loss BELCZYNSKI2010 \
+    "OldWinds_RemMullerMandel": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss-prescription VINK2001 --VMS-mass-loss-prescription VINK2011 --RSG-mass-loss-prescription NJ90 --WR-mass-loss-prescription BELCZYNSKI2010 \
  --remnant-mass-prescription MULLERMANDEL --kick-magnitude-distribution MULLERMANDEL ",
       # no BH kick
-    "OldWinds_RemFryer2012_noBHkick": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss VINK2001 --VMS-mass-loss VINK2011 --RSG-mass-loss NJ90 --WR-mass-loss BELCZYNSKI2010 \
+    "OldWinds_RemFryer2012_noBHkick": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss-prescription VINK2001 --VMS-mass-loss-prescription VINK2011 --RSG-mass-loss-prescription NJ90 --WR-mass-loss-prescription BELCZYNSKI2010 \
  --remnant-mass-prescription FRYER2012 --kick-magnitude-distribution MAXWELLIAN \
  --black-hole-kicks ZERO --kick-magnitude-sigma-CCSN-BH 0 --natal-kick-for-PPISN FALSE ",
       # no NS or BH kick
-    "OldWinds_RemFryer2012_noNSBHkick": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss VINK2001 --VMS-mass-loss VINK2011 --RSG-mass-loss NJ90 --WR-mass-loss BELCZYNSKI2010 \
+    "OldWinds_RemFryer2012_noNSBHkick": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss-prescription VINK2001 --VMS-mass-loss-prescription VINK2011 --RSG-mass-loss-prescription NJ90 --WR-mass-loss-prescription BELCZYNSKI2010 \
  --remnant-mass-prescription FRYER2012 --black-hole-kicks ZERO --kick-magnitude-distribution ZERO --kick-magnitude-sigma-ECSN 0.0 ",
       # NO main sequence mass loss
-    "OldWinds_RemFryer2012_noMSwinds": "--mass-loss-prescription BELCZYNSKI2010 --RSG-mass-loss NJ90 --WR-mass-loss BELCZYNSKI2010 \
+    "OldWinds_RemFryer2012_noMSwinds": "--mass-loss-prescription BELCZYNSKI2010 --RSG-mass-loss-prescription NJ90 --WR-mass-loss-prescription BELCZYNSKI2010 \
  --remnant-mass-prescription FRYER2012 --kick-magnitude-distribution MAXWELLIAN \
- --OB-mass-loss NONE --VMS-mass-loss NONE ",
+ --OB-mass-loss-prescription NONE --VMS-mass-loss-prescription NONE ",
       # NO WR winds
-    "OldWinds_RemFryer2012_noWRwinds": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss VINK2001 --VMS-mass-loss VINK2011 --RSG-mass-loss NJ90 --WR-mass-loss BELCZYNSKI2010 \
+    "OldWinds_RemFryer2012_noWRwinds": "--mass-loss-prescription BELCZYNSKI2010 --OB-mass-loss-prescription VINK2001 --VMS-mass-loss-prescription VINK2011 --RSG-mass-loss-prescription NJ90 --WR-mass-loss-prescription BELCZYNSKI2010 \
  --remnant-mass-prescription FRYER2012 --kick-magnitude-distribution MAXWELLIAN --wolf-rayet-multiplier 0 ",
       # NO winds
     "OldWinds_RemFryer2012_NOwinds": "--mass-loss-prescription BELCZYNSKI2010 --overall-wind-mass-loss-multiplier 0 \
  --remnant-mass-prescription FRYER2012 --kick-magnitude-distribution MAXWELLIAN ",
       # New winds with Fryer remnants
-    "NewWinds_RemFryer2012": "--OB-mass-loss VINK2021 --VMS-mass-loss SABHAHIT2023 --RSG-mass-loss DECIN2023  --WR-mass-loss SANDERVINK2023 \
+    "NewWinds_RemFryer2012": "--OB-mass-loss-prescription VINK2021 --VMS-mass-loss-prescription SABHAHIT2023 --RSG-mass-loss-prescription DECIN2023  --WR-mass-loss-prescription SANDERVINK2023 \
  --remnant-mass-prescription FRYER2012 --kick-magnitude-distribution MAXWELLIAN ",
       # New winds with Muller Mandel remnants
-    "NewWinds_RemMullerMandel": "--OB-mass-loss VINK2021 --VMS-mass-loss SABHAHIT2023 --RSG-mass-loss DECIN2023  --WR-mass-loss SANDERVINK2023 \
+    "NewWinds_RemMullerMandel": "--OB-mass-loss-prescription VINK2021 --VMS-mass-loss-prescription SABHAHIT2023 --RSG-mass-loss-prescription DECIN2023  --WR-mass-loss-prescription SANDERVINK2023 \
  --remnant-mass-prescription MULLERMANDEL --kick-magnitude-distribution MULLERMANDEL ",
        # New winds with Fryer remnants and no BH kick
-    "NewWinds_RemFryer2012_noBHkick": "--OB-mass-loss VINK2021 --VMS-mass-loss SABHAHIT2023 --RSG-mass-loss DECIN2023  --WR-mass-loss SANDERVINK2023 \
+    "NewWinds_RemFryer2012_noBHkick": "--OB-mass-loss-prescription VINK2021 --VMS-mass-loss-prescription SABHAHIT2023 --RSG-mass-loss-prescription DECIN2023  --WR-mass-loss-prescription SANDERVINK2023 \
   --remnant-mass-prescription FRYER2012 --kick-magnitude-distribution MAXWELLIAN \
  --black-hole-kicks ZERO --kick-magnitude-sigma-CCSN-BH 0 --natal-kick-for-PPISN FALSE ",
        # New winds with Fryer remnants and no NS or BH kick
-    "NewWinds_RemFryer2012_noNSBHkick": "--OB-mass-loss VINK2021 --VMS-mass-loss SABHAHIT2023 --RSG-mass-loss DECIN2023  --WR-mass-loss SANDERVINK2023 \
+    "NewWinds_RemFryer2012_noNSBHkick": "--OB-mass-loss-prescription VINK2021 --VMS-mass-loss-prescription SABHAHIT2023 --RSG-mass-loss-prescription DECIN2023  --WR-mass-loss-prescription SANDERVINK2023 \
   --remnant-mass-prescription FRYER2012 --black-hole-kicks ZERO --kick-magnitude-distribution ZERO --kick-magnitude-sigma-ECSN 0.0 ",
 }
